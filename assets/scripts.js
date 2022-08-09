4581b0098c96011dd1ce98284c472fbaf46b6dd2
jc79932 <109046711+jc79932@users.noreply.github.com>
2022-08-09 18:18:25 -0500

    Add files via upload

---
+++ assets/scripts.js
@@ -0,0 +1,13 @@
+/*
+ 
+Greeting/Start: Top-left: Leaderboards. Center: "Welcome to Project Cerulean." Prompt to start game [DONE?]
+--> 
+Countdown replacing Center. "Game starts in: 5". 
+-->  
+Questions begin: Top-left: Leaderboards back. Top-right: Counter (round timer?). Center: Huge container in (Light). Body bg-color in (Primary). Question with clear bg-color. 
+Answer choices in (Primary - Selected, on hover). Wrong takes off 25% of starting time. When answer is correct or time finishes, time takes over center, turns into points. Z-index, 
+background of either blur or opaque color.
+-->
+Finishes, leaderboards appear. Prompts to save score  with initials or retry
+
+*/
