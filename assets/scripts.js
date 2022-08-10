45a8728a0b4d62239c404b3367b3113a70156797
jc79932 <jc79932@gmail.com>
2022-08-10 18:49:43 -0500

    Start button complete, anim

--- assets/scripts.js
+++ assets/scripts.js
@@ -11,9 +11,35 @@ background of either blur or opaque color.
 Finishes, leaderboards appear. Prompts to save score  with initials or retry
 TODO Add transition-bg to igContent, h1 and div-button
 */
+function timer(x,y){
+    let seconds = x;
+    if(x>1 && x<100 &! y==1){
+        var timer = setInterval(function(){
+            console.log(seconds);
+            seconds--;
+        if (seconds < 0) {
+            clearInterval(timer);
+            console.log("Timer out");
+        }
+    }, 1000);} else if(y==1 && x>1 && x<100){
+        var timer = setInterval(function(){
+            console.log("x" + seconds);
+            igContent.style.opacity = seconds / 2;
+            seconds = seconds - 0.1;
+        if (seconds < 0) {
+            igContent.style.opacity = 0;
+            body.innerHTML = leaderBoards;
+            startQuiz();
+            clearInterval(timer);
+            console.log("Timer out");
+        }
+    }, 50);
+    } else{return "Error"}
+}
+
 const body = document.body;
 const igContent = document.createElement("div");
-igContent.innerHTML = "<section id='intro-greetings'><article><h1>Project Cerulean.</h1><div id='button-start' onclick='startQuiz()'>Start Quiz</div></article></section>";
+igContent.innerHTML = "<section id='intro-greetings'><article><h1>Project Cerulean.</h1><div id='button-start' onclick='startQuizAnim()'>Start Quiz</div></article></section>";
 
 const leaderBoards = "<header><nav><h1><a href='#'>Leaderboards</a></h1></nav></header>"
 
@@ -28,13 +54,16 @@ function onloadPage(){
 
 function startQuizAnim(){
    body.id = 'transition-bg';
+   timer(2,1);
+    console.log("Done");
+    
 }
 
 function startQuiz(){
     console.log("test hi");
-    startQuizAnim();
+   
 }
 
 wipePage();
 onloadPage();
-console.log(body);
+console.log(document.getElementsByTagName('h1').id);
