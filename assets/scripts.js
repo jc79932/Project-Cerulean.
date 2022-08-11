ed5fe7ac56e1e16bbaf22df8c8a19c06ed9d6674
jc79932 <jc79932@gmail.com>
2022-08-10 21:44:08 -0500

    Questions JS to HTML

--- assets/scripts.js
+++ assets/scripts.js
@@ -11,6 +11,16 @@ background of either blur or opaque color.
 Finishes, leaderboards appear. Prompts to save score  with initials or retry
 TODO Add transition-bg to igContent, h1 and div-button
 */
+var questions = [
+    ["What is the method that finds elements by element id called?", "document.getElementById(id)","document.getElement(id)", "document.getTheElementById(id)", "document.getElementBy#(id)"], //1
+    ["Which CSS external link tag is correct?","<link href=''stylesheet'' rel=''geeks.css''/>", "<link rel=''stylesheet'' href=''geeks.css''></link>", "<link =''stylesheet'' href=''geeks.css''/>", "<link rel=''stylesheet'' href=''geeks.css''/>"], //4
+    ["Fill the blank: body{________: lightblue}","color","main-color","background-color", "default"], //3
+    ["Which element is positioned relative to the viewport?","Fixed","Absolute","Relative","Static"],//1
+    ["______  is used to create space around an element's content, inside of any defined borders.", "Margin", "Padding", "Space", "Borders"],//2
+];
+let questionNum = 0, answerNum;
+const answerText = document.getElementById('answer');
+const questionText = document.getElementById("question");
 function timer(x,y){
     let seconds = x;
     if(x>1 && x<100 &! y==1){
@@ -24,13 +34,16 @@ function timer(x,y){
     }, 1000);} else if(y==1 && x>1 && x<100){
         var timer = setInterval(function(){
             console.log("x" + seconds);
-            igContent.style.opacity = seconds / 2;
-            seconds = seconds - 0.1;
+            igContent.style.opacity = seconds - 1;
+            seconds = seconds - 0.03;
         if (seconds < 0) {
             igContent.style.opacity = 0;
             body.innerHTML = leaderBoards;
-            startQuiz();
+            
+           
             clearInterval(timer);
+            startQuiz();
+            changeQuiz();
             console.log("Timer out");
         }
     }, 50);
@@ -40,6 +53,8 @@ function timer(x,y){
 const body = document.body;
 const igContent = document.createElement("div");
 igContent.innerHTML = "<section id='intro-greetings'><article><h1>Project Cerulean.</h1><div id='button-start' onclick='startQuizAnim()'>Start Quiz</div></article></section>";
+const questionContent=document.createElement("div");
+questionContent.innerHTML="<section id='quiz-content'><article id = 'question-container'><h1 id='question'></h1><div class='answer' id='one' onclick='checkAnswer(1)'></div><div class='answer' id='two' onclick='checkAnswer(2)'></div><div class='answer'id='three' onclick='checkAnswer(3)'></div><div class='answer' id='four' onclick='checkAnswer(4)'></div></article></section>"
 
 const leaderBoards = "<header><nav><h1><a href='#'>Leaderboards</a></h1></nav></header>"
 
@@ -60,10 +75,62 @@ function startQuizAnim(){
 }
 
 function startQuiz(){
-    console.log("test hi");
-   
+    console.log(questionText);
+    body.append(questionContent);
+    
 }
 
+
+
+function changeQuiz(){
+    const questionText = document.getElementById("question");
+    const answerTextOne = document.getElementById("one");
+    const answerTextTwo = document.getElementById('two');
+    const answerTextThree = document.getElementById('three');
+    const answerTextFour = document.getElementById('four');
+    
+    questionText.innerText = questions[questionNum][0];
+    answerTextOne.innerText = questions[questionNum][1];
+    answerTextTwo.innerText = questions[questionNum][2];
+    answerTextThree.innerText = questions[questionNum][3];
+    answerTextFour.innerText = questions[questionNum][4];
+}
+
+function checkAnswer(z){
+    switch (z){
+        case 1:
+            if(questions[questionNum][1]==questions[0][1] || questions[questionNum][1]==questions[3][1] ){
+                console.log("Hi");
+                questionNum++;
+                changeQuiz();
+                break;}
+                break;
+        case 2:
+            if(questions[questionNum][2]==questions[4][2] ){
+                console.log("Hi");
+                questionNum++;
+                changeQuiz();
+                break;}
+                break;
+        case 3:
+            if(questions[questionNum][3]==questions[2][3] ){
+                console.log("Hi");
+                questionNum++;
+                changeQuiz();
+                break;}
+                break;
+        case 4:
+            if(questions[questionNum][4]==questions[1][4] ){
+                console.log("Hi");
+                 questionNum++;
+                changeQuiz();
+                break;}
+                break;
+        default:
+            console.log("bye");
+            break;
+    }
+}
 wipePage();
 onloadPage();
 console.log(document.getElementsByTagName('h1').id);
